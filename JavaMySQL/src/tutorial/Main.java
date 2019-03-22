7ebb7b9e0d451c820c6c74a0b2c181af787b935c
Nathee Sungthongngam <tonnoidev@gmail.com>
2019-03-22 09:52:09 +0700

    Add project source

---
+++ JavaMySQL/src/tutorial/Main.java
@@ -0,0 +1,78 @@
+package tutorial;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Java connection to MySQL with CRUD Project");
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/testdb", "root", "p@ssw0rd2535");
+            conn.setAutoCommit(false);
+            System.out.println("Open Connection");
+
+            // show all data
+            try (Statement stmt = conn.createStatement()) {
+                try (ResultSet rs = stmt.executeQuery("select * from cars")) {
+                    while (rs.next()) {
+                        System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+                    }
+                }
+            }
+
+            // show all data with header
+            try (Statement stmt = conn.createStatement()) {
+                try (ResultSet rs = stmt.executeQuery("select * from cars")) {
+                    int count = 0;
+                    int cols = 0;
+                    while (rs.next()) {
+                        if (count == 0) {
+                            cols = rs.getMetaData().getColumnCount();
+                            for (int i = 1; i <= cols; i++) {
+                                System.out.print(rs.getMetaData().getColumnName(i) + " ");
+                            }
+                            System.out.println("");
+                            count++;
+                        }
+                        for (int i = 1; i <= cols; i++) {
+                            System.out.print(rs.getString(i) + " ");
+                        }
+                        System.out.println("");
+                    }
+                }
+            }
+            
+            // insert
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate("insert into cars values(8, 'Toyota', 38000)");
+            }
+            
+            // update
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate("update cars set price=90000 where id=8");
+            }
+            
+            // delete
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate("delete from cars where id=8");
+            }
+            
+            conn.commit();
+        } catch (ClassNotFoundException | SQLException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                    System.out.println("Close Connection");
+                } catch (SQLException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # crud-java-mysql
+
+[For manual project]
+Src: Main.java
+
+[For maven project]
+
+Script to run create first maven
+mvn -B archetype:generate 
+  -DarchetypeGroupId=org.apache.maven.archetypes 
+  -DgroupId=com.java.mysql.app 
+  -DartifactId=java-mysql-crud-app
+
+Src: java-mysql-crud-app/
